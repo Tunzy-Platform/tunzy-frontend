d181454487ba843f72140449fa25479b96d657bc
ali sadeghi far <sadeghifarali9@gmail.com>
2026-02-09 00:26:05 +0330

    add settings guard

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import { PlaylistTracksPage } from "./pages/PlaylistTracksPage";
 import { PlayListsPage } from "./pages/PlayListsPage";
+import { SettingsPage } from "./pages/SettingsPage";
+import { SettingsGuard } from "./features/settings/components/SettingsGuard";
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="/playlists" element={<PlayListsPage />} />
-      <Route path="" element={<PlayListsPage />} />
-      <Route path="/playlists/:playlistID" element={<PlaylistTracksPage />} />
+      <Route path="/settings" element={<SettingsPage />} />
+      <Route element={<SettingsGuard />}>
+        <Route path="playlists" element={<PlayListsPage />} />
+        <Route path="" element={<PlayListsPage />} />
+        <Route path="playlists/:playlistID" element={<PlaylistTracksPage />} />
+      </Route>
     </Routes>
   );
 }
