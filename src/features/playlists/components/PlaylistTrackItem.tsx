9494981388ed69f0babe0ea9c7f3620d392e2319
ali sadeghi far <sadeghifarali9@gmail.com>
2026-02-22 13:54:59 +0330

    add download progress

--- src/features/playlists/components/PlaylistTrackItem.tsx
+++ src/features/playlists/components/PlaylistTrackItem.tsx
@@ -13,6 +13,7 @@ import {
 } from "@/features/downloads/hooks";
 import { CloudAlertIcon, CloudDownloadIcon, PlayIcon } from "lucide-react";
 import { ImageSpinnerButton } from "@/components/ImageSpinnerButton";
+import { Progress } from "@/components/ui/progress";
 
 export function PlaylistTrackItem({
   playlistID,
@@ -34,92 +35,110 @@ export function PlaylistTrackItem({
 
   return (
     <Item variant="outline" asChild role="listitem">
-      <div>
-        <ItemMedia variant="image" className="size-16 md:size-16 ">
-          {song.download == null && (
-            <ImageSpinnerButton
-              src={song.thumbnail || ""}
-              alt={song.name}
-              setState={() => downloadMutation(song.id)}
-              disabled={isPending}
-              isLoading={isPending}
-              className="w-16 h-16"
-            >
-              {/* Icon */}
-              <CloudDownloadIcon className="relative z-10 text-white" />
-            </ImageSpinnerButton>
-          )}
-          {/* Successful State */}
-          {song.download &&
-            song.download.status == DownloadStatusEnum.Successful && (
+      <div className="">
+        <div className="flex w-full h-full gap-2 content-center items-around">
+          {/* Thumbnail + Action */}
+          <ItemMedia variant="image" className="size-16 md:size-16 ">
+            {song.download == null && (
               <ImageSpinnerButton
                 src={song.thumbnail || ""}
                 alt={song.name}
-                setState={() => onPlaySong(song)}
+                setState={() => downloadMutation(song.id)}
                 disabled={isPending}
                 isLoading={isPending}
                 className="w-16 h-16"
               >
                 {/* Icon */}
-                <PlayIcon className="relative z-10 text-white" />
+                <CloudDownloadIcon className="relative z-10 text-white" />
               </ImageSpinnerButton>
             )}
-          {/* Pending State */}
-          {song.download &&
-            song.download.status == DownloadStatusEnum.Pending && (
-              <ImageSpinnerButton
-                src={song.thumbnail || ""}
-                alt={song.name}
-                setState={() => null}
-                disabled={isPending}
-                isLoading={true}
-                className="w-16 h-16"
-                children={undefined}
+            {/* Successful State */}
+            {song.download &&
+              song.download.status == DownloadStatusEnum.Successful && (
+                <ImageSpinnerButton
+                  src={song.thumbnail || ""}
+                  alt={song.name}
+                  setState={() => onPlaySong(song)}
+                  disabled={isPending}
+                  isLoading={isPending}
+                  className="w-16 h-16"
+                >
+                  {/* Icon */}
+                  <PlayIcon className="relative z-10 text-white" />
+                </ImageSpinnerButton>
+              )}
+            {/* Pending State */}
+            {song.download &&
+              song.download.status == DownloadStatusEnum.Pending && (
+                <ImageSpinnerButton
+                  src={song.thumbnail || ""}
+                  alt={song.name}
+                  setState={() => null}
+                  disabled={isPending}
+                  isLoading={true}
+                  className="w-16 h-16"
+                  children={undefined}
+                />
+              )}
+            {/* Downloading State */}
+            {song.download &&
+              song.download.status == DownloadStatusEnum.Downloading && (
+                <ImageSpinnerButton
+                  src={song.thumbnail || ""}
+                  alt={song.name}
+                  setState={() => null}
+                  disabled={isPending}
+                  isLoading={true}
+                  className="w-16 h-16"
+                  children={undefined}
+                />
+              )}
+            {/* Failed State */}
+            {song.download &&
+              song.download.status == DownloadStatusEnum.Failed && (
+                <ImageSpinnerButton
+                  src={song.thumbnail || ""}
+                  alt={song.name}
+                  setState={() => {
+                    if (song?.download) retryMutate(song.download.id);
+                  }}
+                  disabled={retryIsPending || isSuccess}
+                  isLoading={retryIsPending || isSuccess}
+                  className="w-16 h-16"
+                >
+                  {/* Icon */}
+                  <CloudAlertIcon className="relative z-10 text-white" />
+                </ImageSpinnerButton>
+              )}
+          </ItemMedia>
+          {/* Title */}
+          <ItemContent className="h-full mt-2 ml-2">
+            <ItemTitle className="line-clamp-1">
+              {song.name} -{" "}
+              <span className="text-muted-foreground">{song.album}</span>
+            </ItemTitle>
+            <ItemDescription>{song.artist_name || ""}</ItemDescription>
+          </ItemContent>
+          {/* Duration */}
+          <ItemContent className="flex-none text-center">
+            <ItemDescription>
+              {convertDurationToTime(song.duration)}
+            </ItemDescription>
+          </ItemContent>
+        </div>
+
+        {/* Download Progress */}
+        {song.download &&
+          song.download.status == DownloadStatusEnum.Downloading && (
+            <div className="flex w-full gap-2 items-center h-1 text-center">
+              <Progress
+                value={song.download.progress}
+                id="progress-upload"
+                className="flex-1 h-1"
               />
-            )}
-          {/* Downloading State */}
-          {song.download &&
-            song.download.status == DownloadStatusEnum.Downloading && (
-              <ImageSpinnerButton
-                src={song.thumbnail || ""}
-                alt={song.name}
-                setState={() => null}
-                disabled={isPending}
-                isLoading={true}
-                className="w-16 h-16"
-                children={undefined}
-              />
-            )}
-          {/* Failed State */}
-          {song.download &&
-            song.download.status == DownloadStatusEnum.Failed && (
-              <ImageSpinnerButton
-                src={song.thumbnail || ""}
-                alt={song.name}
-                setState={() => {
-                  if (song?.download) retryMutate(song.download.id);
-                }}
-                disabled={retryIsPending || isSuccess}
-                isLoading={retryIsPending || isSuccess}
-                className="w-16 h-16"
-              >
-                {/* Icon */}
-                <CloudAlertIcon className="relative z-10 text-white" />
-              </ImageSpinnerButton>
-            )}
-        </ItemMedia>
-        <ItemContent>
-          <ItemTitle className="line-clamp-1">
-            {song.name} -{" "}
-            <span className="text-muted-foreground">{song.album}</span>
-          </ItemTitle>
-          <ItemDescription>{song.artist_name}</ItemDescription>
-        </ItemContent>
-        <ItemContent className="flex-none text-center">
-          <ItemDescription>
-            {convertDurationToTime(song.duration)}
-          </ItemDescription>
-        </ItemContent>
+              <span className="ml-auto text-xs">{song.download.progress}%</span>
+            </div>
+          )}
       </div>
     </Item>
   );
